825bdb511f139bdc6c94e0705336f89df2af47e9
Devesh3293 <96462198+Devesh3293@users.noreply.github.com>
2022-02-03 15:04:22 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,11 @@
+echo I AM NOT RESPONSIBLE FOR ANY DAMAGE CAUSED BY RUNNING THIS SCRIPT. USE AT YOUR OWN RISK!
+echo ==============================
+echo LINUX PACKAGE UPDATER 
+echo ==============================
+echo This script can only update Debian, ubuntu or distros based on them which use APT package manager
+echo ==============================
+echo Updating....
 sudo apt upgrade -y && sudo apt update && sudo apt-get update && sudo apt-get upgrade
+echo Done!
+echo Exiting after 10 seconds
+sleep 10s
