d7c27647f8b9ee5ec0b4307983b73bf4d5b18c6e
Devesh3293 <96462198+themoonmeetsthesun@users.noreply.github.com>
2022-02-21 17:12:09 +0530

    added more stuff

---
+++ updatearchbased.sh
@@ -0,0 +1,19 @@
+clear
+echo I AM NOT RESPONSIBLE FOR ANY DAMAGE CAUSED BY RUNNING THIS SCRIPT. USE AT YOUR OWN RISK!
+echo ==============================
+echo "                    _       _    _           _       _            ";
+echo "     /\            | |     | |  | |         | |     | |           ";
+echo "    /  \   _ __ ___| |__   | |  | |_ __   __| | __ _| |_ ___ _ __ ";
+echo "   / /\ \ | '__/ __| '_ \  | |  | | '_ \ / _\` |/ _\` | __/ _ \ '__|";
+echo "  / ____ \| | | (__| | | | | |__| | |_) | (_| | (_| | ||  __/ |   ";
+echo " /_/    \_\_|  \___|_| |_|  \____/| .__/ \__,_|\__,_|\__\___|_|   ";
+echo "                                  | |                             ";
+echo "                                  |_|                             ";
+echo ==============================
+echo This script can only update manjaro, arch or distros based on them which use pacman
+echo ==============================
+echo updating....
+pacman -Syu
+echo Done!!
+echo Exiting after 5 seconds
+sleep 5s
